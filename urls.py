ba664eec7187c00488089d89b10027fe8fe3eef2
Edward Leming <el230@mps014048.phys.susx.ac.uk>
2015-05-07 20:17:22 +0100

    New files from Phil

--- urls.py
+++ urls.py
@@ -1,22 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
 
-from ukbooking import views
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'),
-                       url(r'^booking/(?P<booking_id>\d+)/$', views.booking, name='booking'),
-                       url(r'^bookings/(?P<year>\d+)/(?P<month>\d+)/$', views.bookings, name='bookings'),
-                       url(r'^bookings/dump/$', views.Bookings.as_view(), name='bookings_dump'),
-                       url(r'^bookings/$', views.bookings, name='bookings'),
-
-
-                       url(r'^book_visit/$', views.book_visit, name='book_visit'),
-
-                       url(r'^visit/(?P<visit_id>\d+)/$', views.visit, name='visit'),
-                       url(r'^visits/(?P<year>\d+)/(?P<month>\d+)/$', views.visits, name='visits'),
-                       url(r'^visits/dump/$', views.Visits.as_view(), name='visits_dump'),
-                       url(r'^visits/$', views.visits, name='visits'),
-
-                       url(r'^apartments/$', views.Apartments.as_view(), name='apartments'),
-                       url(r'^apartment/(?P<apartment_id>\d+)/$', views.apartment, name='apartment'),
+                       url(r'^ukbooking/', include('ukbooking.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
 )
